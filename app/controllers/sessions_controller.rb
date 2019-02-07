72d09254260bd4e8d3200682eae501dce0dd1ef5
rchow1944 <rchow2@buffalo.edu>
2019-02-07 14:29:55 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    #code
+    if params[:name].nil? || params[:name].empty?
+      redirect_to('/sessions/new')
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    #code
+    if !session[:name].nil?
+      session.delete :name
+    end
+    redirect_to '/'
+  end
+end
