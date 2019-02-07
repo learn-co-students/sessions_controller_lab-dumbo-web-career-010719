a40ab8ac7ca7c5b305779b8b65c1c0a67184d579
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-02-07 17:32:31 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+
+  def create
+    if params[:name]== nil || params[:name].empty?
+      redirect_to "/sessions/new"
+    else
+      session[:name]=params[:name]
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    if session[:name]
+       session.delete :name
+    else
+      session[:name]=nil
+    end
+      redirect_to root_path
+  end
+
+end
